a8acb489cdaa582c0da8f27ad4c8a5951a438237
wentan bai <wentan1995@gmail.com>
2022-03-09 20:07:53 +0800

    chore: v3 init

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1646826724141_2584',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
